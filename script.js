cd6fc3b182c3c4a8d99ce605332fcb9c6a049692
Raul Jesus <112392625+rjuzumaki@users.noreply.github.com>
2024-07-30 10:42:25 -0300

    .7

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const caixaprincipal
-const caixaperguntas
-const caixaaltnativa
-const
-const
+const caixaPrincipal=document.queryselector("caixa-principal");
+const caixaPerguntas=document.queryselector("caixa-perguntas");
+const caixaAlternativas=document.queryselector("caixa-alternativas");
+const caixaResultado=document.queryselector("caixa-resultado");
+const caixaResultado=document.queryselector("caixa-resultado");
 
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1"
+        "Alternativa 2"
+    ]
+}
+
+const perguntas = [
+    {
+        enunciados: "Assim que saiu da escola se depara com uma nova tecnologia, que consegue responder todas as perguntas que uma pessos poderia ter, ele também gera áudios hiper-realistas. Qual o seu primeiro pensamento?"
+        alternativas: [
+            "Isso é assustador"
+            "Isso é maravilhoso"
+        ]
+    }
+]
